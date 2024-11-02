09f096ac1642706c5eff8730c24c5147995f8c75
ellisrobertsx <159159321+ellisrobertsx@users.noreply.github.com>
2024-11-02 15:01:37 +0000

    added style to menu page

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Ellis\JR-Catering\JR-Catering\venv
+set VIRTUAL_ENV=C:\Users\Ellis\JR-Catering\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
