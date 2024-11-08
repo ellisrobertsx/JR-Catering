66b6e920ff061be9434940751b2b04a2d80fda45
ellisrobertsx <159159321+ellisrobertsx@users.noreply.github.com>
2024-11-08 16:42:03 +0000

    Added cache busting

--- app.py
+++ app.py
@@ -398,7 +398,6 @@ def optimize_images():
 
 @app.after_request
 def add_headers(response):
-
     response.headers['X-Content-Type-Options'] = 'nosniff'
     response.headers['X-Frame-Options'] = 'SAMEORIGIN'
     response.headers['X-XSS-Protection'] = '1; mode=block'
